12ccf4290ba26aea8b67408d1d2dd739448c0c8a
Luis Salomon Flores Ugalde <vf1barukiri@gmail.com>
2025-08-25 09:53:07 -0600

    samples

--- sample.py
+++ sample.py
@@ -1,11 +1,14 @@
 import sys
 
+
 def print_words():
     if len(sys.argv) == 4:
         print("Your words:", sys.argv[1], sys.argv[2], sys.argv[3])
     else:
         print("Give 3 words!")
 
+
 if __name__ == "__main__":
-    #sys.argv = ["sample.py", "apple", "banana", "cherry"]
+    # sys.argv = ["sample.py", "apple", "banana", "cherry"]
     print_words()
+    # run on terminal : python sample_no_main.py hello world today
--- sample_no_main.py
+++ sample_no_main.py
@@ -4,3 +4,6 @@ if len(sys.argv) == 4:
     print("Your words:", sys.argv[1], sys.argv[2], sys.argv[3])
 else:
     print("Give 3 words!")
+
+
+# run this on terminal: python sample_no_main.py hello world today
