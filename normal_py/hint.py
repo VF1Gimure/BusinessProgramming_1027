f15b90ae59110bc1ef2341eed51c6c59aee1304e
Luis Salomon Flores Ugalde <vf1barukiri@gmail.com>
2025-08-31 23:40:17 -0600

    org

---
+++ normal_py/hint.py
@@ -0,0 +1,15 @@
+import math
+
+math.sqrt(  )
+
+
+
+def greet():
+    name = input("Enter your name: ")
+    return "Hello " + name
+
+def greet(name):
+    return "Hello " + name
+
+print(greet("Luis"))
+print(greet("Maria"))
